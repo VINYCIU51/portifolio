8148176ce46be4eabab76972c10967c9577cace7
Vinycius <vinyciusalves155@gmail.com>
2025-12-27 21:41:21 -0300

    feat: adicionar funcionalidade de ativar e desativar funcao de loop typing

--- src/components/TypingText/TypingText.tsx
+++ src/components/TypingText/TypingText.tsx
@@ -1,58 +1,68 @@
 import { useEffect, useState } from "react";
-import  styles from "./TypingText.module.css";
+import styles from "./TypingText.module.css";
 
 interface TypingProps {
-  text: string[];
-  speed?: number;
-  finalDelay?: number;
+    text: string[];
+    speed?: number;
+    finalDelay?: number;
+    loop?: boolean;
+    showCursor?: boolean;
 }
 
-export const TypingText = ({ text, speed = 100, finalDelay= 400 }: TypingProps) => {
-  const [displayText, setDisplayText] = useState("");
-  const [charIndex, setCharIndex] = useState(0);
-  const [currentTextIndex, setCurrentTextIndex] = useState(0);
-  const [isDeleting, setIsDeleting] = useState(false);
+export const TypingText = ({
+    text,
+    speed = 100,
+    finalDelay = 400,
+    loop = false,
+}: TypingProps) => {
+    const [displayText, setDisplayText] = useState("");
+    const [charIndex, setCharIndex] = useState(0);
+    const [currentTextIndex, setCurrentTextIndex] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
 
-  const currentText = text[currentTextIndex];
+    const currentText = text[currentTextIndex];
 
-  useEffect(() => {
-    const timeout = setTimeout(() => {
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            if (!isDeleting) {
+                if (charIndex < currentText.length) {
+                    setDisplayText((prev) => prev + currentText[charIndex]);
+                    setCharIndex((prev) => prev + 1);
+                    return;
+                }
 
-      if (!isDeleting && charIndex < currentText.length) {
-        setDisplayText(prev => prev + currentText[charIndex]);
-        setCharIndex(prev => prev + 1);
-        return;
-      }
+                if (charIndex === currentText.length && loop === true) {
+                    setTimeout(() => {
+                        setIsDeleting(true);
+                    }, finalDelay);
+                    return;
+                }
+            }
 
-      if (!isDeleting && charIndex === currentText.length) {
-        setTimeout(() => {
-          setIsDeleting(true);
-        },finalDelay);
-        return;
-      }
+            if (isDeleting) {
+                if (charIndex > 0) {
+                    setDisplayText(currentText.slice(0, charIndex - 1));
+                    setCharIndex((prev) => prev - 1);
+                    return;
+                }
 
-      if (isDeleting && charIndex > 0) {
-        setDisplayText(currentText.slice(0, charIndex - 1));
-        setCharIndex(prev => prev - 1);
-        return;
-      }
+                if (isDeleting && charIndex === 0) {
+                    setTimeout(() => {
+                        setIsDeleting(false);
+                        setCurrentTextIndex((prev) => (prev + 1) % text.length);
+                    }, finalDelay);
+                    return;
+                }
+            }
+        }, speed);
 
-      if (isDeleting && charIndex === 0) {
-        setTimeout(() => {
-          setIsDeleting(false);
-          setCurrentTextIndex(prev => (prev + 1) % text.length);
-        },finalDelay);
-        return;
-      }
-    }, speed);
+        return () => clearTimeout(timeout);
+    }, [charIndex, isDeleting, currentText, speed, text.length]);
 
-    return () => clearTimeout(timeout);
-  }, [charIndex, isDeleting, currentText, speed, text.length]);
-
-  return (
-    <span className={styles.wrapper}>
-      <span className={styles.text}>{displayText}</span>
-      <span className={styles.cursor}></span>
-    </span>
-  );
+    return (
+        <span className={styles.wrapper}>
+            <span className={styles.text}>{displayText}</span>
+            <span className={styles.cursor}></span>
+        </span>
+    );
 };
