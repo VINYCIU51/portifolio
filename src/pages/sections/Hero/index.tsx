91ca6456e1c03742d60cb789f2877a8df3c6b7a7
Vinycius <vinyciusalves155@gmail.com>
2025-12-28 19:29:44 -0300

    refactor: remover itens nao utilizados

--- src/pages/sections/Hero/index.tsx
+++ src/pages/sections/Hero/index.tsx
@@ -1,27 +1,39 @@
-import { ProfilePhoto } from "../../../components/ProfilePhoto/ProfilePhoto";
-import Photo from "../../../assets/images/gitProfile.webp";
 import "./style.css";
-import  {TypingText }  from "../../../components/TypingText/TypingText";
+import { TypingText } from "../../../components/TypingText/TypingText";
+import FloatingLines from "../../../components/FloatingLines/FloatingLines";
 
-const  Hero = () => {
+const Hero = () => {
+    return (
+        <>
+            <div
+                style={{ width: "100%", height: "100%", position: "absolute" }}
+            >
+                <FloatingLines
+                    linesGradient={["#6e6e6e", "#525252"]}
+                    enabledWaves={["top", "middle"]}
+                    // Array - specify line count per wave; Number - same count for all waves
+                    lineCount={[12, 6, 20]}
+                    // Array - specify line distance per wave; Number - same distance for all waves
+                    lineDistance={[50, 25, 4]}
+                    bendRadius={5.0}
+                    bendStrength={-0.6}
+                    interactive={true}
+                    parallax={false}
+                    mixBlendMode="difference"
+                />
+            </div>
 
-  return (
-    <>
-    <section>
+            <section>
+                <div className="title">
+                    <h2>Hi, I am a</h2>
+                    <TypingText
+                        text={["FullStack Developer", "Problem Solver", "dor"]}
+                        loop={false}
+                    />
+                </div>
+            </section>
+        </>
+    );
+};
 
-      <div className="title">
-        <h2>Hi, I am a</h2>
-        <TypingText text={["FullStack Developer", "Problem Solver", "dor"]} />
-      </div>
-
-      <div className="title-complement">
-        <ProfilePhoto  path={Photo} size="30dvw" />
-      </div>
-
-    </section>
-
-    </>
-  )
-}
-
-export default Hero
+export default Hero;
